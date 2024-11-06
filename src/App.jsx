7dd1f4f77cfead7024d4bb2f985b87f5cc34557c
Zairaxx <brandontsegai@gmail.com>
2024-11-06 14:55:28 +0100

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import EmployeeList from './components/EmployeeList'
 
 function App() {
 
+  //Exempel på två states
+  // name (med default-värdet Markus)
+  // showList (med default-värdet true)
   let [name, setName] = useState("Markus");
   let [showList, setShowList] = useState(true);
   
@@ -19,10 +22,12 @@ function App() {
 
   let handleChange = () => {
     let newName = document.querySelector("#nameInput").value;
+    //Funktion för att ändra på name-state till nytt namn från input
     setName(newName);
   }
 
   let toggleList = () => {
+    //Ändrar på showList mellan true/false
     setShowList(!showList);
   }
 
